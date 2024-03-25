23f35461c5058c652b3cb4b175e9a62e95671313
Tejas Pawar <tejaspawar70238@gmail.com>
2024-03-25 13:24:44 +0530

    Create home.js

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+function updateCountdown() {
+
+    var targetDate = new Date("2024-03-31T00:00:00");
+    var currentDate = new Date();
+
+    var timeDifference = targetDate.getTime() - currentDate.getTime();
+
+    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    var countdownTimer = document.getElementById("countdown-timer");
+    if(timeDifference<0){
+        countdownTimer.textContent= "Expired";
+    }else countdownTimer.textContent = days + " : " + hours + " : " + minutes + " : " + seconds;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
